0a91a1836fbcfe07eda20dc0732143ca668ec21c
kalis <kalis@localhost.localdomain>
2024-08-24 03:58:24 +0530

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,35 +1,31 @@
 import sys
+import re
+
 
 # import pyparsing - available if you need it!
 # import lark - available if you need it!
 
 
 def match_pattern(input_line, pattern):
-    if len(pattern) == 1:
-        return pattern in input_line
-    elif pattern == "\\d":
+    if pattern == r"\w":
+        return bool(re.search(r'\w', input_line))
+    elif pattern == r"\d":
         return any(c.isdigit() for c in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
-
 def main():
-    pattern = sys.argv[2]
-    input_line = sys.stdin.read()
-
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
+    if len(sys.argv) != 3 or sys.argv[1] != "-E":
+        print("Usage: ./your_program.sh -E <pattern>")
         exit(1)
 
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+    pattern = sys.argv[2]
+    input_line = sys.stdin.read().strip()
 
-    # Uncomment this block to pass the first stage
     if match_pattern(input_line, pattern):
         exit(0)
     else:
         exit(1)
-
 
 if __name__ == "__main__":
     main()
