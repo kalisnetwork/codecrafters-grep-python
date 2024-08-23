3699dd83eb43c25266727a350a4a1236be3e8375
kalis <kalis@localhost.localdomain>
2024-08-24 03:50:01 +0530

    pass 2st stage

--- app/main.py
+++ app/main.py
@@ -7,6 +7,8 @@ import sys
 def match_pattern(input_line, pattern):
     if len(pattern) == 1:
         return pattern in input_line
+    elif pattern == "\\d":
+        return any(c.isdigit() for c in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
